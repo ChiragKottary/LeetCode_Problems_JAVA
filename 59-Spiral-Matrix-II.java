d3b879309acfd23cf1cd1cf0c74c5e326f1eb785
Chirag Kottary <83589681+ChiragKottary@users.noreply.github.com>
2024-06-08 15:51:20 +0530

    [59] [Time Beats: 100.00%] [Memory Beats: 35.11%] - LeetPush

---
+++ 59-Spiral-Matrix-II.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] arr = new int[n][n];
+        List<Integer> list = new ArrayList<>();
+
+        int cols = n;
+        int rows = n;
+        int k = 0;
+        int left = 0 ;
+        int right = cols - 1;
+        int up = 0;
+        int down = rows - 1;
+
+        int count = 0;
+
+        while( count < (n*n))
+        {
+            for(int col = left; col <= right; col++)
+            {
+                arr[up][col] = ++k;
+                count++;
+            }
+            //traversal from up to down
+            for(int row = up + 1; row <= down; row++)
+            {
+                arr[row][right] = ++k;
+                count++;
+            }
+
+            if(up != down)
+            {
+            //traversal from right to left
+            for(int col = right - 1; col >=left; col--)
+            {
+                arr[down][col] = ++k;
+                count++;
+            }
+            }
+            if(left != right)
+            {
+            //traversal from down to up
+            for(int row = down - 1; row >up; row--)
+            {
+                arr[row][left] = ++k;
+                count++;
+            }
+            }
+            left++;
+            right--;
+            up++;
+            down--;
+        }
+        
+
+     return arr;   
+    }
+}
