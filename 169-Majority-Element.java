870f2b56b9121df829dadf87d9d7307a9b0c6b17
Chirag Kottary <83589681+ChiragKottary@users.noreply.github.com>
2024-06-14 15:49:50 +0530

    [169] [Time Beats: undefined] [Memory Beats: undefined] - LeetPush

---
+++ 169-Majority-Element.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i = 0; i < n;i++ )
+        {
+            int value = map.getOrDefault(nums[i], 0);
+            map.put(nums[i], value + 1);
+        }
+
+        for(Map.Entry<Integer,Integer> eni: map.entrySet())
+        {
+            if(eni.getValue() > (n/2))
+            {
+                 return eni.getKey();
+            }
+        }
+        // for (Map.Entry<Integer, Integer> it : map.entrySet()) {
+        //     if (it.getValue() > (n / 2)) {
+        //         return it.getKey();
+        //     }
+        // }
+        return -1;
+    }
+}
