860084c118f259e879e7f4e62f296d9df55ec9ce
Chirag Kottary <83589681+ChiragKottary@users.noreply.github.com>
2024-07-07 10:40:57 +0530

    [1518] [Time Beats: 100.00%] [Memory Beats: 38.70%] - LeetPush

--- 1518-Water-Bottles.java
+++ 1518-Water-Bottles.java
@@ -1,15 +1,14 @@
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
         int rem= 0;
-        int full=0;
         int ans = numBottles;
 
         while(numBottles >= numExchange)
         {
             rem = numBottles%numExchange;
-            full = numBottles/numExchange;
-            ans += full;
-            numBottles = full+rem;
+            numBottles = numBottles/numExchange;
+            ans += numBottles;
+            numBottles = numBottles+rem;
         }
     return ans ;   
     }
