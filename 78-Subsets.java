1920956c5b90b7b30ab956de17886f4841e744c7
Chirag Kottary <83589681+ChiragKottary@users.noreply.github.com>
2024-07-27 19:10:12 +0530

    [78] [Time Beats: 53.58%] [Memory Beats: 5.53%] - LeetPush

---
+++ 78-Subsets.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> ans =new ArrayList<>();
+    public void helper(int[] nums , int index,List<Integer> Combi){
+        if(index == nums.length){
+            ans.add(new ArrayList<>(Combi));
+            return;
+        }
+        //Dont include in the list
+        helper(nums,index+1,Combi);
+        
+        //Element include in the list
+        Combi.add(nums[index]);
+        helper(nums,index+1,Combi);
+        Combi.remove(Combi.size()-1);
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        helper(nums,0,list);
+     return ans;   
+    }
+}
